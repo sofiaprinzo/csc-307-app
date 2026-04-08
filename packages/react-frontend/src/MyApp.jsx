f0b87b30164487ba0001393af1ea7431c4cdd16e
sofiaprinzo <sprinzo@calpoly.edu>
2026-04-08 13:21:59 -0700

    Added Form

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,27 +1,13 @@
 // src/MyApp.jsx
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 
 
 function MyApp() {
   const [characters, setCharacters] = useState([
-    {
-        name: "Charlie",
-        job: "Janitor",
-    },
-    {
-        name: "Mac",
-        job: "Bouncer",
-    },
-    {
-        name: "Dee",
-        job: "Aspiring actress",
-    },
-    {
-        name: "Dennis",
-        job: "Bartender",
-    },
+
   ])
 
   function removeOneCharacter(index) {
@@ -35,6 +21,7 @@ function MyApp() {
     return (
     <div className="container">
       <Table characterData = {characters}  removeCharacter = {removeOneCharacter} />
+      <Form />
     </div>
   );
 }

0d894d3df99fa59ac0449241962b03da55ec7cec
sofiaprinzo <sprinzo@calpoly.edu>
2026-04-12 15:44:34 -0700

    added POST /users route.

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -45,6 +45,13 @@ const findUserByName = (name) => {
 const findUserById = (id) =>
   users["users_list"].find((user) => user["id"] === id);
 
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+
+
 
 // routes
 
@@ -71,6 +78,12 @@ app.get("/users/:id", (req, res) => {
   } else {
     res.send(result);
   }
+})
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
 })
 
 

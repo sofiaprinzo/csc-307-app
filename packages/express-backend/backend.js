30e0611dbc040fd474faffb16b4ed5ac30438f14
sofiaprinzo <sprinzo@calpoly.edu>
2026-04-26 14:09:23 -0700

    IE4 completed.

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,6 +1,21 @@
 // backend.js
 import express from "express";
 import cors from "cors";
+import userServices from "./services/user-service.js";
+
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const { MONGO_CONNECTION_STRING } = process.env;
+
+mongoose.set("debug", true);
+mongoose
+  .connect(MONGO_CONNECTION_STRING + "users") // connect to Db "users"
+  .catch((error) => console.log(error));
+
+
 
 const app = express();
 const port = 8000;
@@ -8,65 +23,7 @@ const port = 8000;
 app.use(cors());
 app.use(express.json());
 
-const users = {
-    users_list: [
-      {
-        id: "xyz789",
-        name: "Charlie",
-        job: "Janitor",
-      },
-      {
-        id: "abc123",
-        name: "Mac",
-        job: "Bouncer",
-      },
-      {
-        id: "ppp222",
-        name: "Mac",
-        job: "Professor",
-      },
-      {
-        id: "yat999",
-        name: "Dee",
-        job: "Aspring actress",
-      },
-      {
-        id: "zap555",
-        name: "Dennis",
-        job: "Bartender",
-      },
-    ],
-};
 
-// helper functions
-
-const findUserByName = (name) => {
-  return users["users_list"].filter((user)=> user["name"] === name);
-};
-
-const findUserById = (id) =>
-  users["users_list"].find((user) => user["id"] === id);
-
-const addUser = (user) => {
-  users["users_list"].push(user);
-  return user;
-};
-
-const removeUserById = (id) => {
-  const index = users["users_list"].findIndex((user) => user["id"] === id);
-  if (index === -1) {
-    return undefined;
-  }
-  return users["users_list"].splice(index, 1)[0];
-};
-
-const findUserByNameAndJob = (name, job) => {
-  return users["users_list"].filter(
-    (user) => user["name"] === name && user["job"] === job
-  );
-};
-
-const generateID = () => Math.random().toString();
 
 
 
@@ -80,49 +37,64 @@ app.get("/", (req, res) => {
 app.get("/users", (req, res) => {
   const name = req.query.name;
   const job = req.query.job;
-
-  if (name != undefined && job != undefined) {
-    let result = findUserByNameAndJob(name, job);
-    result = { users_list: result };
-    res.send(result);
-  } else if (name != undefined) {
-    let result = findUserByName(name);
-    result = { users_list : result }
-    res.send(result);
-  } else {
-    res.send(users);
-  }
+  
+  userServices
+    .getUsers(name, job)
+    .then((result) => {
+      res.send({users_list: result});
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    }); 
 });
 
 app.get("/users/:id", (req, res) => {
   const id = req.params["id"];
-  let result = findUserById(id);
-  if (result === undefined) {
-    res.status(404).send("Resource not found.");
-  } else {
-    res.send(result);
-  }
-})
+  
+  userServices
+    .findUserById(id)
+    .then((result => {
+      if (result == null) {
+        res.status(404).send("Resource not found.");
+      } else {
+        res.send(result);
+      }
+    }))
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
 
 app.post("/users", (req, res) => {
   const userToAdd = req.body;
-  const newUser = {
-    ...userToAdd,
-    id: generateID(),
-  };
-  addUser(newUser);
-  res.status(201).send(newUser);
-})
+  
+  userServices
+    .addUser(userToAdd)
+    .then((newUser) => {
+      res.status(201).send(newUser);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+
+});
 
 app.delete("/users/:id", (req, res) => {
   const id = req.params["id"];
-  const result = removeUserById(id)
 
-  if (result === undefined) {
-    res.status(404).send("Resource not found.");
-  } else {
-    res.status(204).send();
-  }
+  userServices
+    .removeUser(id)
+    .then((result) => {
+      if (result == null) {
+        res.status(404).send("Resource not found.")
+      } else {
+        res.status(204).send();
+      }
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+
 });
 
 
--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -14,7 +14,7 @@ function MyApp() {
     deleteUser(id)
       .then((response) => {
         if (response.status === 204) {
-          const updated = characters.filter((character) => character.id !== id);
+          const updated = characters.filter((character) => character._id !== id);
           setCharacters(updated); 
         } else if (response.status === 404) {
           console.log("User not found.");
